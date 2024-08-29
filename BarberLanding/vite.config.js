61124d6ff6e7a4e5c81519d8da53458c819f7beb
Thyale MD <thyaledantas@gmail.com>
2024-08-29 11:55:52 -0300

    Uptade img e mudança no vite

--- BarberLanding/vite.config.js
+++ BarberLanding/vite.config.js
@@ -5,4 +5,8 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   base: '/LandingPageBarbearia/',
   plugins: [react()],
+  build: {
+    publicDir: 'public',
+    assetsInclude: ['public/**/*'],
+  },
 })
