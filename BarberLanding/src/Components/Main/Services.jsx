b284ddda8520e66b5dc0e4e78e791e10d174719b
Thyale MD <thyaledantas@gmail.com>
2024-08-29 10:10:01 -0300

    Services criado

---
+++ BarberLanding/src/Components/Main/Services.jsx
@@ -0,0 +1,15 @@
+import './Services.scss'
+
+export default function Services (){
+    return(
+        <section className='Services'>
+            <h1 className='Services__title'>SERVIÇOS</h1>
+            <p className='Services__text'>"Descubra uma experiência completa de cuidado e estilo na [Nome da Barbearia]. Nossos serviços vão além do corte de cabelo, incluindo cortes clássicos e modernos, tratamentos de barba e capilares. Nossa equipe experiente garante que você saia com um visual renovado e confiante."</p>
+            <div className='Services__DivServices'>
+                <div></div>
+                <div></div>
+                <div></div>
+            </div>
+        </section>
+    )
+}
--- BarberLanding/src/main.jsx
+++ BarberLanding/src/main.jsx
@@ -5,11 +5,13 @@ import '../src/main.scss'
 import Top from '../src/Components/Header/Top'
 import Sobre from '../src/Components/Main/Sobre'
 import BtnWhats from './Components/IconWhats/BtnWhats'
+import Services from './Components/Main/Services'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Top/>
     <Sobre/>
     <BtnWhats/>
+    <Services/>
   </StrictMode>,
 )
